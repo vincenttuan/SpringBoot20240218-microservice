c90b44f4e171be303ce7ba8720e85f4ab1bef877
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-24 21:46:47 +0800

    java

---
+++ client-access-micro-service-8080/src/main/java/com/example/demo/ClientAccessMicroService8080Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAccessMicroService8080Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAccessMicroService8080Application.class, args);
+	}
+
+}
