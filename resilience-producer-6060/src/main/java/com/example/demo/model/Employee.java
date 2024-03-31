ecfe4ff7d39bb71b938dcf5f1192f441882dc184
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-31 19:32:14 +0800

    java

---
+++ resilience-producer-6060/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	private String empId;
+	private String empName;
+	private String designation;
+	private Double salary;
+}
