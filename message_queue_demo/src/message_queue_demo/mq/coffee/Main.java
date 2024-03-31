8cc90dcff6dcc089cb8f86ba6b0d84c4ee8487f9
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-31 21:12:52 +0800

    java

---
+++ message_queue_demo/src/message_queue_demo/mq/coffee/Main.java
@@ -0,0 +1,28 @@
+package message_queue_demo.mq.coffee;
+
+import java.util.Random;
+
+public class Main {
+	
+	public static void main(String[] args) throws InterruptedException {
+		MessageQueue queue = new MessageQueue();
+		
+		// 啟動消費者執行緒
+		Consumer consumer = new Consumer(queue);
+		new Thread(consumer).start();
+		
+		
+		// 啟動生產者執行緒
+		Producer producer = new Producer(queue);
+		// 當咖啡庫存大於 0 時, 持續購買咖啡
+		while (true) {
+			// 模擬購買咖啡的動作
+			if(producer.getCoffeeStock() > 0) {
+				new Thread(producer).start();
+			}
+			Thread.sleep(new Random().nextInt(5000));
+		}
+		
+	}
+	
+}
