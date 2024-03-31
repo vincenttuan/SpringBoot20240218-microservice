3e615ff582f857e7f62f6e52e4a3fed3e139ce83
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-31 21:30:46 +0800

    java

--- message_queue_demo/src/message_queue_demo/mq/quote/Main.java
+++ message_queue_demo/src/message_queue_demo/mq/quote/Main.java
@@ -7,10 +7,12 @@ public class Main {
 		MessageQueue queue = new MessageQueue();
 		
 		StockPriceProducer producer1 = new StockPriceProducer(queue, "2330", 600, 700);
+		StockPriceProducer producer2 = new StockPriceProducer(queue, "1101", 40, 50);
 		
 		StockPriceConsumer consumer = new StockPriceConsumer(queue);
 		
 		new Thread(producer1).start();
+		new Thread(producer2).start();
 		new Thread(consumer).start();
 		
 	}
