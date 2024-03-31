d24437f78d3d8753867b88af6c7996b8324da5d2
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-31 20:40:12 +0800

    java

---
+++ message_queue_demo/src/message_queue_demo/mq/coffee/MessageQueue.java
@@ -0,0 +1,27 @@
+package message_queue_demo.mq.coffee;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MessageQueue {
+	private Queue<String> queue = new LinkedList<>();
+	
+	// 生產消息
+	public synchronized void produce(String message) {
+		queue.add(message); // 將消息加入到佇列
+		notifyAll(); // 通知消費者
+	}
+	
+	// 消費消息
+	public synchronized String consume() {
+		while (queue.isEmpty()) {
+			try {
+				System.out.println("消費者等待中...");
+				this.wait(); // 如果佇列是空的就等待消息
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return queue.poll(); // 從佇列中"取出"消息
+	}
+}
