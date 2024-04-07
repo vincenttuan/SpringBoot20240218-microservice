077d514c20c294079b631cac098cdb071ee2ccf1
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-07 20:28:17 +0800

    java

--- rabbitmq-stream-twstock-starter-9901/src/main/java/com/example/demo/service/PriceEmitterService.java
+++ rabbitmq-stream-twstock-starter-9901/src/main/java/com/example/demo/service/PriceEmitterService.java
@@ -19,7 +19,7 @@ public class PriceEmitterService {
 	private RabbitTemplate rabbitTemplate;
 	
 	@Autowired
-	private FanoutExchange fanoutExchange;
+	private FanoutExchange stockExchange;
 	
 	// 停止標記
 	public boolean stop; // 預設是 false
@@ -28,10 +28,44 @@ public class PriceEmitterService {
 	@EventListener(ContextRefreshedEvent.class)
 	public void emitPrices() {
 		try {
+			String[] jsonStrs = getJsonArray();
+			
+			for(String json : jsonStrs) {
+				String message = json.trim();
+				// 判斷是否停止
+				if(stop || message.contains("\"symbol\":\"000000\"")) {
+					break;
+				}
+				
+				if(message.length() < 10 ) {
+					continue;
+				}
+				
+				String routingKey = null;
+				if(stockExchange.getType().equals("fanout")) {
+					// FanoutExchange
+					routingKey = "";
+				} else {
+					// DirectExchange
+					routingKey = "tw.stock";
+				}
+				
+				// 發送消息
+				rabbitTemplate.convertAndSend(stockExchange.getName(), routingKey, message);
+				
+				System.out.println("發送: " + message);
+				
+				Thread.sleep(10);
+			}
+			System.out.println("發送: 報價停止 !");
 			
 		} catch(Exception e) {
-			
+			e.printStackTrace();
 		}
+	}
+	
+	public void stop() {
+		this.stop = true;
 	}
 	
 	// 取得 price.json 並轉 String[]
