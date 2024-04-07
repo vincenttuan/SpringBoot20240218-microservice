cd8e1d506e41f238172b434d06ba653c20315ed1
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-07 21:04:25 +0800

    java

---
+++ rabbitmq-stream-twstock-consumer-9902/src/main/java/com/example/demo/RabbitmqStreamTwstockConsumer9902Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqStreamTwstockConsumer9902Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqStreamTwstockConsumer9902Application.class, args);
+	}
+
+}
