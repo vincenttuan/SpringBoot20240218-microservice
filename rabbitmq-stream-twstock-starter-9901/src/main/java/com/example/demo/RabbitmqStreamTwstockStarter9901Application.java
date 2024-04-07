f1f94256cd8fadcc074a97568b5c237c033373c5
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-07 19:53:11 +0800

    java

---
+++ rabbitmq-stream-twstock-starter-9901/src/main/java/com/example/demo/RabbitmqStreamTwstockStarter9901Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqStreamTwstockStarter9901Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqStreamTwstockStarter9901Application.class, args);
+	}
+
+}
